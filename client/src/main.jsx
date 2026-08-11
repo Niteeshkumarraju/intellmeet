b5ea90b06845eeb324fd8d1b6ea1fc5dc2183ebf
Niteesh Kumar Raju <niteeshkumarraju0011@gmail.com>
2026-08-11 19:21:57 +0530

    Add Axios 401 response interceptor for auto-logout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,22 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
+import useAuthStore from './store/authStore'
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || ''
 
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      useAuthStore.getState().logout()
+      if (window.location.pathname !== '/login' && window.location.pathname !== '/signup') {
+        window.location.href = '/login'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
